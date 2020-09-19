e9a117a06e208db6ab52876a954b633f52d47236
Without Boats <woboats@gmail.com>
2020-09-19 14:02:50 +0200

    write to stdout with io-uring

--- examples/read.rs
+++ examples/read.rs
@@ -6,6 +6,15 @@ fn main() {
         let mut file: File<maglev::Driver> = File::open_on_driver("props.txt", maglev::Driver::default()).await.unwrap();
         let mut buf = vec![0; 4096];
         let end = file.read(&mut buf[..]).await.unwrap();
-        println!("{}", String::from_utf8_lossy(&buf[0..end]));
+
+        buf.truncate(end);
+
+        let (_, result) = maglev::Driver::default().submit(event::Write {
+            fd: 1,
+            buf: buf.into_boxed_slice(),
+            offset: 0,
+        }).await;
+
+        result.unwrap();
     });
 }
