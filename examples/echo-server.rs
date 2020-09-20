65e80cd9de8ef3d47a1f3d058d0058e7efbafe31
Without Boats <woboats@gmail.com>
2020-09-20 14:28:36 +0200

    echo server

---
+++ examples/echo-server.rs
@@ -0,0 +1,22 @@
+use ringbahn::net::TcpListener;
+use maglev::Driver;
+
+use futures::StreamExt;
+use futures::io::{AsyncReadExt, AsyncWriteExt};
+
+fn main() {
+    maglev::block_on(async {
+        let mut listener = TcpListener::bind_on_driver("127.0.0.1:7878", Driver::default()).unwrap();
+        listener.incoming_no_addr().for_each_concurrent(16, |stream| async {
+            let mut stream = stream.unwrap();
+            let mut bytes = Vec::from(&b"echo> "[..]);
+            let len = bytes.len();
+            bytes.extend(&[0; 1024][..]);
+            stream.write_all(b"Hello, world!\n").await.unwrap();
+            loop {
+                let n = stream.read(&mut bytes[len..]).await.unwrap();
+                stream.write_all(&bytes[..(len + n)]).await.unwrap();
+            }
+        }).await;
+    })
+}
